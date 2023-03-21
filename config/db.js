138caac8fad381ffcc42c24df94c2c829a1a9043
Arjun E <arjunrk907@gmail.com>
2023-03-21 21:55:11 +0530

    fix a minor flaw

--- config/db.js
+++ config/db.js
@@ -4,8 +4,13 @@ import mongoose from "mongoose";
  * Connected to Mongodb
  */
 export const connectedToDb = () => {
-  const db = process.env.MONGO_DB;
-  mongoose.connect(db).then(() => {
-    console.log("connected to mongodb");
-  });
+  const db = process.env.MONGODB_KEY;
+  mongoose
+    .connect(db)
+    .then(() => {
+      console.log("connected to mongodb");
+    })
+    .catch((err) => {
+      console.log("mongodb connection error !!!!");
+    });
 };
--- route/api.js
+++ route/api.js
@@ -2,6 +2,6 @@ import express from "express";
 import { urlShortner } from "../controller/shortner.js";
 const router = express.Router();
 
-router.post("/", urlShortner);
+router.post("/shrink", urlShortner);
 
 export default router;
