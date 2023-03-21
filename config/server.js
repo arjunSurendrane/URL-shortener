9d468be3a2a13424beedc82571853efc118e4350
Arjun E <arjunrk907@gmail.com>
2023-03-21 21:15:46 +0530

    update port number

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@
  * @param {Function} app - express()
  */
 export const connectedToLocalhost = (app) => {
-  const PORT = process.env.PORT || 8000;
+  const PORT = process.env.PORT || 6000;
   app.listen(PORT, (err) => {
     if (err) return console.log(`failed to connect PORT-${PORT}`);
     console.log(`connected to PORT-${PORT}`);
